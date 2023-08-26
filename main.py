fa1caddeb7c60448c7219da6ed35efdbe86d2e5d
TwistBuff <108197775+TwistBuff@users.noreply.github.com>
2023-08-26 20:13:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,4 +37,77 @@ def france():
     done() 
 
 
-france()
+def ivory_coast():
+    for i in range(2):
+        forward(270)
+        right(90)
+        forward(270/2)
+        right(90)
+    
+    fillcolor("orange")
+    begin_fill()
+    
+    for i in range(2):
+        forward(270/3)
+        right(90)
+        forward(270/2)
+        right(90)
+    
+    
+    end_fill()
+
+    forward(270)
+
+    fillcolor("green")
+    begin_fill()
+
+    for i in range(2):
+        right(90)
+        forward(270/2)
+        right(90)
+        forward(270/3)
+    
+    end_fill()
+
+
+
+    done() 
+
+
+
+
+def italy():
+    for i in range(2):
+        forward(270)
+        right(90)
+        forward(270/2)
+        right(90)
+    
+    fillcolor("green")
+    begin_fill()
+    
+    for i in range(2):
+        forward(270/3)
+        right(90)
+        forward(270/2)
+        right(90)
+    
+    
+    end_fill()
+
+    forward(270)
+
+    fillcolor("red")
+    begin_fill()
+
+    for i in range(2):
+        right(90)
+        forward(270/2)
+        right(90)
+        forward(270/3)
+    
+    end_fill()
+
+
+
+    done()
